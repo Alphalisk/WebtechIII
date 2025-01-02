164bd1cc54f2cd985d55aeb8aa09cac62346ed19
Richard <richard-mank@hotmail.nl>
2025-01-02 16:31:11 +0100

    Vos Api toegevoegd.

--- Week2/MemoryPlaatjes/modules/api.js
+++ Week2/MemoryPlaatjes/modules/api.js
@@ -10,11 +10,18 @@ export async function fetchImages(source, count) {
         // Dog API voor hondenfoto's
         const response = await fetch(`https://dog.ceo/api/breeds/image/random/${count}`);
         const data = await response.json();
-        urls = data.message; // Array met afbeeldingslinks
+        urls = data.message;
     } else if (source === "cat") {
         // Cataas API voor kattenfoto's
         for (let i = 0; i < count; i++) {
             urls.push(`https://cataas.com/cat?${Math.random()}`); // Cache busten met een query string
+        }
+    } else if (source === "fox") {
+        // Random Fox API voor vossenfoto's
+        for (let i = 0; i < count; i++) {
+            const response = await fetch("https://randomfox.ca/floof/");
+            const data = await response.json();
+            urls.push(data.image); // Voeg de afbeelding toe aan de lijst
         }
     }
 
