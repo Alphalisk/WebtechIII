e3e9e05db42dd85aa900f5e106fcebf105b61e2b
Richard <richard-mank@hotmail.nl>
2025-01-12 17:38:53 +0100

    Cross site problemen opgelost.

--- Memory3/memory_frontend/modules/fetch.js
+++ Memory3/memory_frontend/modules/fetch.js
@@ -1,15 +1,18 @@
 // Bewaar de originele fetch
 const originalFetch = window.fetch;
 
-// Overschrijf de fetch-functie
+// Overschrijf de fetch-functie alleen als dat nodig is
 window.fetch = async function (url, options = {}) {
-    // Zorg dat headers bestaan
-    options.headers = options.headers || {};
+    const isAuthRequired = url.startsWith('http://localhost:8000'); // Pas alleen aan voor interne API's
 
-    // Voeg de Authorization-header toe als een JWT bestaat
-    const token = localStorage.getItem('jwt');
-    if (token) {
-        options.headers['Authorization'] = `Bearer ${token}`;
+    if (isAuthRequired) {
+        options.headers = options.headers || {};
+
+        // Voeg de Authorization-header toe als een JWT bestaat
+        const token = localStorage.getItem('jwt');
+        if (token) {
+            options.headers['Authorization'] = `Bearer ${token}`;
+        }
     }
 
     // Roep de originele fetch-functie aan
